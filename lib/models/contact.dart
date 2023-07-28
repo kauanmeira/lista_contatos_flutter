8f740dc342bdc7d647714ddb7d057ee163c7925b
kauan.meira <kauangabriel.meira123@gmail.com>
2023-07-28 17:03:49 -0300

    implementação da lista de contatos na Tela inicial

---
+++ lib/models/contact.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nomeColumn = "nomeColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null && _db!.isOpen) {
+      return _db!;
+    } else {
+      _db = await initDb();
+      return _db!;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'contacts.db');
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute('''
+        CREATE TABLE $contactTable (
+          $idColumn INTEGER PRIMARY KEY,
+          $nomeColumn TEXT,
+          $emailColumn TEXT,
+          $phoneColumn TEXT,
+          $imgColumn TEXT
+        )
+      ''');
+    });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact?> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map<String, dynamic>> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nomeColumn, emailColumn, phoneColumn, imgColumn],
+        where: '$idColumn = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    return await dbContact
+        .delete(contactTable, where: '$idColumn = ?', whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: '$idColumn', whereArgs: [contact.id]);
+  }
+
+  Future<List<Contact>> getAllContacts() async {
+    Database dbContact = await db;
+    List<Map<String, dynamic>> listMap =
+        await dbContact.rawQuery('SELECT * FROM $contactTable');
+    List<Contact> listContact = [];
+    for (Map<String, dynamic> m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+
+  Future<int?> getNumber() async {
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(
+        await dbContact.rawQuery('SELECT COUNT (*) FROM $contactTable'));
+  }
+
+  Future close() async {
+    Database dbContact = await db;
+    dbContact.close();
+  }
+}
+
+class Contact {
+  int? id;
+  String nome;
+  String email;
+  String phone;
+  String img;
+
+  Contact({
+    this.id,
+    required this.nome,
+    required this.email,
+    required this.phone,
+    required this.img,
+  });
+
+  Contact.fromMap(Map<String, dynamic> map)
+      : id = map[idColumn],
+        nome = map[nomeColumn],
+        email = map[emailColumn],
+        phone = map[phoneColumn],
+        img = map[imgColumn];
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      nomeColumn: nome,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img,
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return 'Contact(id: $id, nome: $nome, email: $email, phone: $phone, img: $img)';
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:lista_contatos/models/contact.dart';
+import 'package:path/path.dart';
+import 'package:flutter/painting.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  Future<void> _initData() async {
+    Contact c = Contact(
+      nome: 'Kauan meira',
+      email: 'kauan@gmail.com',
+      phone: '17988214036',
+      img: 'imgtest',
+    );
+
+    await helper.saveContact(c);
+
+    helper.getAllContacts().then((list) {
+      print(list);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contatos'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Row(children: [
+          Container(
+            width: 80.0,
+            height: 80.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: contacts[index].img != null
+                      ? FileImage(File(contacts[index].img))
+                      : AssetImage('images/image.pgn')
+                          as ImageProvider<Object>),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  contacts[index].nome ?? '',
+                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  contacts[index].email ?? '',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                Text(
+                  contacts[index].phone ?? '',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
